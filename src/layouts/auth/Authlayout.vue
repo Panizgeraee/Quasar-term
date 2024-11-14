a27dbfaeaf83799558085e4e167d6bc8f865560b
unknown <you@example.com>
2024-11-14 17:56:49 +0330

    first commit

---
+++ src/layouts/auth/Authlayout.vue
@@ -0,0 +1,7 @@
+<template>
+    <q-layout class="bg-primary">
+        <router-view></router-view>
+    </q-layout>
+</template>
+<script lang="ts" setup></script>
+<style></style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,8 +3,8 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () => import('layouts/auth/authlayout.vue'),
+    children: [{ path: 'loggin', component: () => import('pages/auth/Loginpage.vue') }],
   },
 
   // Always leave this as last one,
